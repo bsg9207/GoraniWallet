d20d619e1b0ff9b8882abf28cc009b2fe874729b
Seunggwan Back <bsg920729@gmail.com>
2022-11-26 16:42:45 +0900

    add component for selecting network

---
+++ front/src/component/networkSelector.js
@@ -0,0 +1,87 @@
+// Created by Seunggwan, Back on 2022.11.26
+// Copyright (C) 2022-2022 Seunggwan, Back - All Rights Reserved
+import { Select } from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux/es/exports";
+import ChainList from "../../../common/blockchain/network/chainList.js";
+import { eNetworkType } from "../../../common/blockchain/network/network.js";
+import { setNetwork } from "../reducer/networkReducer.js";
+
+const NetworkSelector = () => {
+  // state
+  const [optionsList, setOptionsList] = useState();
+
+  const dispatch = useDispatch();
+  const onSetNetwork = (network) => dispatch(setNetwork(network));
+
+  // effect
+  useEffect(() => {
+    async function init() {
+      const optionsList = createOptions();
+
+      setOptionsList(optionsList);
+    }
+    init();
+  }, []);
+
+  // on change
+  const onChange = (value) => {
+    console.log("selected:", value);
+    onSetNetwork(value);
+  };
+
+  // create options
+  const createOptions = () => {
+    let optionsKlaytn = [];
+    let optionsHitop = [];
+    for (let network in eNetworkType) {
+      // pass unknwon
+      if (eNetworkType[network] === eNetworkType.Unknown) {
+        continue;
+      }
+
+      // hitop
+      if (eNetworkType[network] === eNetworkType.Hitop) {
+        optionsHitop.push(network);
+        continue;
+      }
+
+      // klaytn
+      optionsKlaytn.push(network);
+    }
+
+    let optionsList = [];
+    for (let chain in ChainList) {
+      let options = [];
+      if (chain === "Klaytn") {
+        for (let idx in optionsKlaytn) {
+          const network = optionsKlaytn[idx];
+          options.push({ label: network, value: network, id: idx });
+        }
+      } else if (chain === "Hitop") {
+        for (let idx in optionsHitop) {
+          const network = optionsHitop[idx];
+          options.push({ label: network, value: network, id: idx });
+        }
+      }
+
+      optionsList.push({
+        label: chain,
+        options: options,
+      });
+    }
+
+    return optionsList;
+  };
+
+  return (
+    <Select
+      defaultValue="Cypress"
+      style={{ width: 150 }}
+      onChange={onChange}
+      options={optionsList}
+    ></Select>
+  );
+};
+
+export default NetworkSelector;
