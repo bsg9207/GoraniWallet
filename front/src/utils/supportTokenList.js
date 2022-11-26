ee76fee17909db0188f4fe19d509822cf354b650
Seunggwan Back <bsg920729@gmail.com>
2022-11-26 17:38:35 +0900

    add supported token contract address list

---
+++ front/src/utils/supportTokenList.js
@@ -0,0 +1,13 @@
+// Created by Seunggwan, Back on 2022.11.26
+// Copyright (C) 2022-2022 Seunggwan, Back - All Rights Reserved
+const list = {
+  oEthereum: "0x34d21b1e550d73cee41151c77f3c73359527a396",
+  oUsdt: "0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167",
+  oUsdc: "0x754288077d0ff82af7a5317c7cb8c444d421d103",
+  kDai: "0x5c74070fdea071359b86082bd9f9b3deaafbe32b",
+  KSP: "0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654",
+  vKSP: "0x2f3713f388bc4b8b364a7a2d8d57c5ff4e054830",
+  oORC: "0xfe41102f325deaa9f303fdd9484eb5911a7ba557",
+};
+
+export default list;
