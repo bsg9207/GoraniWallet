be26d205f5e0081c44eae6072adf40e91ec4035f
Seunggwan Back <bsg920729@gmail.com>
2022-11-26 17:37:47 +0900

    enable caver-js module

--- front/config-overrides.js
+++ front/config-overrides.js
@@ -1,7 +1,22 @@
 const { removeModuleScopePlugin } = require("customize-cra");
+const webpack = require("webpack");
 
 // override webpack config
 module.exports = function override(config, env) {
+  // enable caver-js module
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    assert: require.resolve('assert'),
+    buffer: require.resolve('buffer'),
+    crypto: require.resolve("crypto-browserify"),
+    fs: false,
+    https: require.resolve("https-browserify"),
+    http: require.resolve("stream-http"),
+    os: require.resolve("os-browserify"),
+    stream: require.resolve("stream-browserify"),
+    url: require.resolve("url"),
+  };
+
   // enable importing from outside src directory
   if (!config.plugins) {
     config.plugins = [];
